654dbd35172cb768765d4ff5ce1810a5c6f90a1e
Nadun Malinda <bknandun@gmail.com>
2024-10-14 13:35:05 +0200

    remove(playwright): intention to bring it back

--- client/tests/e2e/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Should navigate to home", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Epidemic Sound Code Interview");
-
-  await page.click("text=Playlists");
-  await expect(page).toHaveURL("/playlists");
-});
